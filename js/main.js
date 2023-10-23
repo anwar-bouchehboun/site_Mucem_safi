e3c44b1d44e70f41709f0976091dab8f569ee734
anouar <anouar.ab95@gmail.com>
2023-10-23 22:52:58 +0100

    slide and regex

--- js/main.js
+++ js/main.js
@@ -31,57 +31,78 @@ faqItem.forEach((item) => {
 function add() {
     var email = document.getElementById("email").value;
     var phone = document.getElementById("tele").value;
-     var cin = document.getElementById("cin").value;
-     var selectElement = document.getElementById("type");
-     var selectedValue = selectElement.value;
-    var cop=0;
+    var cin = document.getElementById("cin").value;
   
-    var regexcin =/^[A-Z]{2}[0-9]+$/
+    var selectElement = document.getElementById("type");
+    var selectedValue = selectElement.value;
+    var cop = 0;
+  
+    var regexcin = /^[A-Z]{2}[0-9]+$/;
     var regxEmail = /^[A-Za-z0-9_.]+@([a-z]+\.)+[a-z]{2,3}$/;
     var regexPhone = /^\+212[0-9]{9}$/;
-    //cin validation 
-    if(cin ==""){
-        alert( "Please fill in this cin field.");
-        cop++;
-    }else if(!regexcin.test(cin)){
-        alert( "Invalid cin ");
-        cop++;
-    } 
-    // Email validation
-    if (email == "") {
-    alert( "Please fill in this email field.");
-    cop++;
-    } else if (!regxEmail.test(email)) {
-      alert("Invalid email address.");
+  
+    // cin validation
+    if (cin === "" || !regexcin.test(cin)) {
+      document.getElementById("erreurcin").innerHTML = "*";
+      document.getElementById("erreurcin").style.color = 'red';
       cop++;
-    } 
-
+    }
+else{
+    document.getElementById("erreurcin").innerHTML = "";
+}
+  
+    // Email validation
+    if (email === "" || !regxEmail.test(email)) {
+      document.getElementById("erreuremail").innerHTML = "*";
+      document.getElementById("erreuremail").style.color = 'red';
+      cop++;
+    }else{
+        document.getElementById("erreuremail").innerHTML = "";
+    }
   
     // Phone validation
-    if (phone == "") {
-    alert(   "Please fill in this phone field.");
-    cop++;
-    } else if (!regexPhone.test(phone)) {
-    alert( "Invalid phone number. It should start with '+212' and have 9 digits.");
-    cop++;
+    if (phone === "" || !regexPhone.test(phone)) {
+      document.getElementById("erreurtele").innerHTML = "*";
+      document.getElementById("erreurtele").style.color = 'red';
+      cop++;
+    }else{
+        document.getElementById("erreurtele").innerHTML = "";
     }
-//   type validation 
- if (selectedValue === "") {
-    alert("Please select a type");
-    cop++;
-} 
-
- if(cop==0){
-    alert("msg enj");
-}
-
   
+    // Type validation
+    if (selectedValue === "Type Atelier") {
+      document.getElementById("erreurtype").innerHTML = "*";
+      document.getElementById("erreurtype").style.color = 'red';
+      cop++;
+    }
   
-
+    if (cop === 0) {
      
+         document.getElementById("MSSAGE").innerHTML="Message Env";
+         document.getElementById("email").value = "";
+    document.getElementById("tele").value = "";
+    document.getElementById("erreurtype").innerHTML = "";
+    document.getElementById("type").value = "Type Atelier";
+    
+    }
+  
+  }
+  
+     
+
+
+//slide images
+var i=0;
+var image=['css/imggallery/poterie_safi_maalam4.png','css/imggallery/artiste 2.jpg','css/imggallery/poterie_tuille_argil6-2.png'];
+var time=3000;
+function change(){
+    document.slide.src=image[i];
+    if(i<image.length-1){
+        i++;
+    }else{
+        i=0;
+    }
+    setTimeout("change()",time);
 }
-
-
-
-
+window.onload=change;
    
